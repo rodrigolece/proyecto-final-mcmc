1d4cf550a1ad37b2aac88ab7a46bf5526f46c165
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2015-06-03 00:30:25 -0500

    Sólo una función para subir y bajar H

--- histeresis.jl
+++ histeresis.jl
@@ -1,6 +1,6 @@
 module Histeresis
 
-export MicroEstado, edo_inicial, magnetizacion_aumenta_H, magnetizacion_disminuye_H, microEstados_aumenta_H
+export MicroEstado, edo_inicial, edo_aleatorio, magnetizacion_aumenta_H!
 
 type MicroEstado
     σ::Array{Int,2}
@@ -13,82 +13,63 @@ import Base.show
 
 show(io::IO, m::MicroEstado) = print(io, m.σ)
 
-function edo_inicial(L::Int)
-    σ = -ones(Int, (L,L))
+function edo_inicial(L::Int, R, espin::Int)
+    σ = espin*ones(Int, (L,L))
 
     h = Array(Float64, (L,L))
     for i in 1:L^2
-        # Temporal, hay que cambiar la distribución
-        h[i] = randn()
+        h[i] = R*randn()
     end
 
     MicroEstado(σ, h, L)
 end
 
+function edo_aleatorio(L::Int, R)
+    σ = rand(0:1, (L,L))*2 - 1
 
-function energia_espin(m::MicroEstado, i::Int, j::Int)
+    h = Array(Float64, (L,L))
+    for i in 1:L^2
+        h[i] = R*randn()
+    end
+
+    MicroEstado(σ, h, L)
+end
+
+function criterio_espin(m::MicroEstado, i::Int, j::Int)
     σ = m.σ ; L = m.L
 
-    #Tal vez deberíamos de cambiar el signo de la energía
-    σ[i,j]*( σ[mod1(i-1, L),j] + σ[mod1(i+1, L),j] + σ[i,mod1(j-1, L)] + σ[i,mod1(j+1, L)] ) +
-    m.h[i,j]
+    σ[mod1(i-1, L),j] + σ[mod1(i+1, L),j] + σ[i,mod1(j-1, L)] + σ[i,mod1(j+1, L)] + m.h[i,j]
 end
 
-function voltea_espin_abajo!(m::MicroEstado, i::Int, j::Int)
-    if m.σ[i,j] == -1
-        m.σ[i,j] *= -1
-    end
+function voltea_espin!(m::MicroEstado, i::Int, j::Int)
+    m.σ[i,j] *= -1
 end
 
-function voltea_espin_arriba!(m::MicroEstado, i::Int, j::Int)
-    if m.σ[i,j] == 1
-        m.σ[i,j] *= -1
-    end
-end
-
-function max_energia_abajo(m::MicroEstado,min_energia)
-    energias = Array(Float64,(m.L,m.L))
+function max_abajo(m::MicroEstado, valor_min, espin::Int)
+    tmp = Array(Float64,(m.L,m.L))
 
     for i = 1:m.L, j = 1:m.L
-        if m.σ[i,j] == -1
-            energias[i,j] = energia_espin(m,i,j)
+        if m.σ[i,j] == espin
+            tmp[i,j] = -espin*criterio_espin(m,i,j)
         else
-            energias[i,j] = min_energia
+            tmp[i,j] = valor_min
         end
     end
 
-    #Si cambiamos signo de la energía sería findmin
-    f = findmax(energias) # Da el máximo y el índice lineal del máximo
+    f = findmax(tmp) # Da el máximo y el índice lineal del máximo
+	# Traducimos el índice lineal a dos índices matriciales
     i = mod1(f[2], m.L)
     j = int(ceil(f[2]/m.L))
 
-    -f[1], i, j
+    espin*f[1], i, j
 end
 
-function min_energia_arriba(m::MicroEstado,max_energia)
-    energias = Array(Float64,(m.L,m.L))
-
-    for i = 1:m.L, j = 1:m.L
-        if m.σ[i,j] == 1
-            energias[i,j] = energia_espin(m,i,j)
-        else
-            energias[i,j] = max_energia
-        end
-    end
-
-    f = findmin(energias) # Da el máximo y el índice lineal del máximo
-    i = mod1(f[2], m.L)
-    j = int(ceil(f[2]/m.L))
-
-    f[1], i, j
-end
-
-function espines_vecinos_abajo(m::MicroEstado,i::Int,j::Int)
+function espines_vecinos_abajo(m::MicroEstado, i::Int, j::Int, espin::Int)
     vecinos = [ (mod1(i-1,m.L),j), (mod1(i+1,m.L),j), (i,mod1(j-1,m.L)), (i,mod1(j+1,m.L)) ]
     vecinos_abajo = (Int,Int)[]
 
     for k in vecinos
-        if m.σ[k...] == -1
+        if m.σ[k...] == espin
             push!(vecinos_abajo, k)
         end
     end
@@ -96,58 +77,22 @@ function espines_vecinos_abajo(m::MicroEstado,i::Int,j::Int)
     vecinos_abajo
 end
 
-function espines_vecinos_arriba(m::MicroEstado,i::Int,j::Int)
-    vecinos = [ (mod1(i-1,m.L),j), (mod1(i+1,m.L),j), (i,mod1(j-1,m.L)), (i,mod1(j+1,m.L)) ]
-    vecinos_arriba = (Int,Int)[]
-
-    for k in vecinos
-        if m.σ[k...] == 1
-            push!(vecinos_arriba, k)
-        end
-    end
-
-    vecinos_arriba
-end
-
-function avalancha_abajo(m::MicroEstado, i::Int, j::Int, H::Float64)
-    voltea_espin_abajo!(m,i,j)
-    #puede haber un problema, si se escoje de entrada un espín que ya está volteado
+function avalancha_abajo!(m::MicroEstado, i::Int, j::Int, H::Float64, espin::Int)
+    voltea_espin!(m,i,j)
     espines_volteados = [(i,j)]
 
-    candidatos = espines_vecinos_abajo(m,i,j)
+    candidatos = espines_vecinos_abajo(m,i,j,espin)
 
     while length(candidatos) > 0
         nuevos_candidatos = (Int,Int)[]
 
         for k in candidatos
-            if energia_espin(m,k...) < H # H es negativo en un principio
-                voltea_espin_abajo!(m,k...)
-                push!(espines_volteados,k)
-                nuevos_candidatos = vcat(nuevos_candidatos, espines_vecinos_abajo(m,k...))
-            end
-        end
-
-        candidatos = nuevos_candidatos
-    end
-
-    m, espines_volteados
-end
-
-function avalancha_arriba(m::MicroEstado, i::Int, j::Int, H::Float64)
-    voltea_espin_arriba!(m,i,j)
-    #puede haber un problema, si se escoje de entrada un espín que ya está volteado
-    espines_volteados = [(i,j)]
-
-    candidatos = espines_vecinos_arriba(m,i,j)
-
-    while length(candidatos) > 0
-        nuevos_candidatos = (Int,Int)[]
-
-        for k in candidatos
-            if energia_espin(m,k...) > H # H es positivo en un principio
-                voltea_espin_arriba!(m,k...)
-                push!(espines_volteados,k)
-                nuevos_candidatos = vcat(nuevos_candidatos, espines_vecinos_abajo(m,k...))
+            if m.σ[k...] == espin
+                if  -espin*(criterio_espin(m,k...) + H) > 0
+                    voltea_espin!(m,k...)
+                    push!(espines_volteados,k)
+                    nuevos_candidatos = vcat(nuevos_candidatos, espines_vecinos_abajo(m,k...,espin))
+                end
             end
         end
 
@@ -159,54 +104,37 @@ end
 
 magnetizacion(m::MicroEstado) = sum(m.σ)
 
-function magnetizacion_aumenta_H(m::MicroEstado)
-    energia_minima = minimum(m.h)-5
+function magnetizacion_aumenta_H!(m::MicroEstado, H_set, espin::Int)
+    valor_min = -(maximum(abs(m.h)))-5
     N = m.L^2
     mag = [magnetizacion(m)]
-    hs = [0.]
 
-    while mag[end] < N
-        H,i,j = max_energia_abajo(m,energia_minima)
+    H,i,j = max_abajo(m, valor_min, espin)
+    hs = [H]
+
+    while -espin*(hs[end] - H_set) < 0 && mag[end] != -espin*N
         push!(hs, H)
-        m, volteados = avalancha_abajo(m,i,j,H)
-        ΔM = 2*length(volteados)
-        #Ojo: esto sólo sirve porque volteamos espines hacia arriba
+        m, volteados = avalancha_abajo!(m,i,j,H, espin)
+        ΔM = -2espin*length(volteados)
         push!(mag, mag[end] + ΔM)
+        H,i,j = max_abajo(m, valor_min, espin)
     end
 
     mag, hs
 end
 
-function magnetizacion_disminuye_H(m::MicroEstado)
-    max_energia = maximum(m.h)+5
-    N = m.L^2
-    mag = [magnetizacion(m)]
-    hs = [0.]
+# function microEstados_aumenta_H(m::MicroEstado, num_pasos::Int)
+#     energia_minima = minimum(m.h)-5
+#     edos = Array{Int,2}[copy(m.σ)] #Si no se hace ésto, nada más se copia el apuntador
+#     sizehint(edos, num_pasos)
 
-    while mag[end] > -N
-        H,i,j = min_energia_arriba(m,max_energia)
-        push!(hs, H)
-        m, volteados = avalancha_arriba(m,i,j,H)
-        ΔM = -2*length(volteados)
-        #Ojo: esto sólo sirve porque volteamos espines hacia abajo
-        push!(mag, mag[end] + ΔM)
-    end
+#     for i in 1:num_pasos-1
+#         H,i,j = max_energia_abajo(m,energia_minima)
+#         avalancha_abajo(m,i,j,H)
+#         push!(edos, copy(m.σ))
+#     end
 
-    mag, hs
-end
-
-function microEstados_aumenta_H(m::MicroEstado, num_pasos::Int)
-    energia_minima = minimum(m.h)-5
-    edos = Array{Int,2}[copy(m.σ)] #Si no se hace ésto, nada más se copia el apuntador
-    sizehint(edos, num_pasos)
-
-    for i in 1:num_pasos-1
-        H,i,j = max_energia_abajo(m,energia_minima)
-        avalancha_abajo(m,i,j,H)
-        push!(edos, copy(m.σ))
-    end
-
-    edos
-end
+#     edos
+# end
 
 end
